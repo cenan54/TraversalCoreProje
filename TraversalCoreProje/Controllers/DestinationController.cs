ea907915e8c3613507553e4b064537161f44e6f8
Mustafa Cenan <54mustafacenan54@gmail.com>
2024-12-02 23:20:11 +0300

    made some changes on theme revised layout components (navbar, footer etc.)

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,16 +1,29 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
 {
     public class DestinationController : Controller
     {
-        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal()); 
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
         }
     }
 }
